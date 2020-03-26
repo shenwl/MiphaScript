0e47063cd2961cc87a39c28e635e13bcea985c49
shenwl <shenweilun96@gmail.com>
2020-03-26 23:06:22 +0800

    实现支持peek操作的Iterator

---
+++ src/main/java/common/PeekIterator.java
@@ -0,0 +1,66 @@
+package common;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.stream.Stream;
+
+public class PeekIterator<T> implements Iterator<T> {
+    private final static int CACHE_SIZE = 10;
+    private Iterator<T> iterator;
+    private LinkedList<T> queueCache;
+    private LinkedList<T> stackPutBack;
+
+    public PeekIterator(Stream<T> stream) {
+        iterator = stream.iterator();
+        queueCache = new LinkedList<>();
+        stackPutBack = new LinkedList<>();
+    }
+
+    public T peek() {
+        // 如果putBack栈中有指，就拿第一个
+        if (stackPutBack.size() > 0) {
+            return this.stackPutBack.getFirst();
+        }
+        if (!iterator.hasNext()) {
+            return null;
+        }
+        T val = next();
+        this.putBack();
+        return val;
+    }
+
+    /**
+     * 将缓存数据放回流
+     * 缓存：A -> B -> C -> D
+     * 放回：D -> C -> B -> A
+     */
+    public void putBack() {
+        if (queueCache.size() > 0) {
+            // 把缓存的最后一项拿出来，放入stackPutBack；
+            stackPutBack.push(queueCache.pollLast());
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        // putBack栈里有元素或者流中有元素
+        return stackPutBack.size() > 0 || iterator.hasNext();
+    }
+
+    @Override
+    public T next() {
+        T val = null;
+        // 优先拿stackPutBack里的数据，没有了再去流里拿
+        if (stackPutBack.size() > 0) {
+            val = stackPutBack.pop();
+        } else {
+            val = iterator.next();
+        }
+        // 当缓存大于最大缓存 - 1时，把链表第一个元素（最先入队缓存的）移除，把位置留给next要拿出的元素
+        while (queueCache.size() > CACHE_SIZE - 1) {
+            queueCache.poll();
+        }
+        queueCache.add(val);
+        return val;
+    }
+}
