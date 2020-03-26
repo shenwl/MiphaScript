750a0403dae0a07ffe4bc1b174a27f0c0e23e6f1
shenwl <shenweilun96@gmail.com>
2020-03-26 23:28:52 +0800

    test_endToken

--- src/test/java/common/PeekIteratorTest.java
+++ src/test/java/common/PeekIteratorTest.java
@@ -8,7 +8,7 @@ public class PeekIteratorTest {
     @Test
     public void test_peek() {
         String source = "ashdkhasldhaksdhkajhdkjsadgkjsahdjkas";
-        PeekIterator<Character> peekIterator = new PeekIterator<>(source.chars().mapToObj(c -> (char)c));
+        PeekIterator<Character> peekIterator = new PeekIterator<>(source.chars().mapToObj(c -> (char) c));
 
         assertEquals('a', peekIterator.next());
         assertEquals('s', peekIterator.next());
@@ -22,9 +22,9 @@ public class PeekIteratorTest {
     }
 
     @Test
-    public void test_lookahead2() {
+    public void test_putBack() {
         String source = "abcdef";
-        PeekIterator<Character> peekIterator = new PeekIterator<>(source.chars().mapToObj(c -> (char)c));
+        PeekIterator<Character> peekIterator = new PeekIterator<>(source.chars().mapToObj(c -> (char) c));
         assertEquals('a', peekIterator.next());
         assertEquals('b', peekIterator.next());
         assertEquals('c', peekIterator.next());
@@ -35,5 +35,20 @@ public class PeekIteratorTest {
         assertEquals('b', peekIterator.next());
         assertEquals('c', peekIterator.next());
 
+    }
+
+    @Test
+    public void test_endToken() {
+        String source = "abcdef";
+        PeekIterator<Character> peekIterator = new PeekIterator<>(source.chars().mapToObj(c -> (char) c), (char) 0);
+        int i = 0;
+        while (peekIterator.hasNext()) {
+            if (i == 6) {
+                assertEquals((char) 0, peekIterator.next());
+            } else {
+                assertEquals(source.charAt(i), peekIterator.next());
+                i++;
+            }
+        }
     }
 }
