9d1eb057b05f7527dc9e640996f05fa2e0d899df
shenwl <shenweilun96@gmail.com>
2020-04-04 00:55:33 +0800

    更多复杂表达式测试

--- src/test/java/parser/ParseExprTest.java
+++ src/test/java/parser/ParseExprTest.java
@@ -33,4 +33,16 @@ public class ParseExprTest {
         ASTNode expr = createExpr("\"1\" == \"\"");
         assertEquals("\"1\" \"\" ==", ParserUtils.toPostfixExpression(expr));
     }
+
+    @Test
+    public void complex() throws LexicalException, ParserException {
+        ASTNode expr1 = createExpr("1+2*3");
+        ASTNode expr2 = createExpr("1*2+3");
+        ASTNode expr3 = createExpr("10*(7+4)");
+        ASTNode expr4 = createExpr("(1*2!=7)==3!=4*5+6");
+        assertEquals("1 2 3 * +", ParserUtils.toPostfixExpression(expr1));
+        assertEquals("1 2 * 3 +", ParserUtils.toPostfixExpression(expr2));
+        assertEquals("10 7 4 + *", ParserUtils.toPostfixExpression(expr3));
+        assertEquals("1 2 * 7 != 3 4 5 * 6 + != ==", ParserUtils.toPostfixExpression(expr4));
+    }
 }
