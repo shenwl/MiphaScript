ac175d71bc397e9be8ec1a67cc6ac2393747cc4e
shenwl <shenweilun96@gmail.com>
2020-04-05 21:49:31 +0800

    parseStmt

--- src/main/java/parser/ast/Stmt.java
+++ src/main/java/parser/ast/Stmt.java
@@ -1,7 +1,27 @@
 package parser.ast;
 
+import lexer.Token;
+import parser.utils.ParserException;
+import parser.utils.PeekTokenIterator;
+
 public abstract class Stmt extends ASTNode {
     public Stmt(ASTNode parent, ASTNodeTypes type, String label) {
         super(parent, type, label);
+    }
+
+    public static ASTNode parseStmt(ASTNode parent, PeekTokenIterator it) throws ParserException {
+        // 解析语句需要往前看两个
+        Token token = it.next();
+        Token lookahead = it.next();
+        it.putBack();
+
+        if(token.isVariable() && lookahead.getValue().equals("=")) {
+            return AssignStmt.parse(parent, it);
+        }
+        if(token.getValue().equals("var")) {
+            return DeclareStmt.parse(parent, it);
+        }
+        // TODO: 其他case
+        return null;
     }
 }
