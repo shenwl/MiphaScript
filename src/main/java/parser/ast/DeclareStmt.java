1a19c5f8302d3ea1c12f113ad347e965e7c97cc5
shenwl <shenweilun96@gmail.com>
2020-04-05 21:43:08 +0800

    Parse DeclareStmt & test

--- src/main/java/parser/ast/DeclareStmt.java
+++ src/main/java/parser/ast/DeclareStmt.java
@@ -1,7 +1,29 @@
 package parser.ast;
 
+import lexer.Token;
+import parser.utils.ParserException;
+import parser.utils.PeekTokenIterator;
+
 public class DeclareStmt extends Stmt {
     public DeclareStmt(ASTNode parent) {
         super(parent, ASTNodeTypes.DECLARE_STMT, "declare");
+    }
+
+    public static ASTNode parse(ASTNode parent, PeekTokenIterator it) throws ParserException {
+        ASTNode stmt = new DeclareStmt(parent);
+        it.nextMatch("var");
+        // 匹配（变量）factor
+        Token token = it.peek();
+        ASTNode factor = Factor.parse(parent, it);
+        if (factor == null) {
+            throw new ParserException(token);
+        }
+        stmt.addChild(factor);
+        // 匹配 "=" 后面的表达式
+        Token lexeme = it.nextMatch("=");
+        ASTNode expr = Expr.parse(parent, it);
+        stmt.addChild(expr);
+        stmt.setLexeme(lexeme);
+        return stmt;
     }
 }
---
+++ src/test/java/parser/DeclareStmtTest.java
@@ -0,0 +1,29 @@
+package parser;
+
+import exceptions.LexicalException;
+import lexer.Lexer;
+import lexer.Token;
+import org.junit.jupiter.api.Test;
+import parser.ast.ASTNode;
+import parser.ast.DeclareStmt;
+import parser.utils.ParserException;
+import parser.utils.ParserUtils;
+import parser.utils.PeekTokenIterator;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeclareStmtTest {
+    private PeekTokenIterator createTokenIt(String src) throws LexicalException {
+        Lexer lexer = new Lexer();
+        ArrayList<Token> tokens = lexer.analyse(src.chars().mapToObj(c -> (char)c));
+        return new PeekTokenIterator(tokens.stream());
+    }
+    @Test
+    public void test_declareStmtParse() throws LexicalException, ParserException {
+        PeekTokenIterator it = createTokenIt("var i = 100 * 2");
+        ASTNode stmt = DeclareStmt.parse(null, it);
+        assertEquals("i 100 2 * =", ParserUtils.toPostfixExpression(stmt));
+    }
+}
