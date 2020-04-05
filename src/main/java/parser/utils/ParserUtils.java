dc11950a8c10484c9f826665b3a85682baa934f0
shenwl <shenweilun96@gmail.com>
2020-04-05 21:42:08 +0800

    支持DECLARE_STMT

--- src/main/java/parser/utils/ParserUtils.java
+++ src/main/java/parser/utils/ParserUtils.java
@@ -10,6 +10,7 @@ public class ParserUtils {
 
         switch (node.getType()) {
             case BINARY_EXPR:
+            case DECLARE_STMT:
                 leftStr = toPostfixExpression(node.getChild(0));
                 rightStr = toPostfixExpression(node.getChild(1));
                 return leftStr + " " + rightStr + " " + node.getLexeme().getValue();
