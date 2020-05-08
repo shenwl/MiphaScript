20e0ede93213713bf89f90dfed54a0413b748225
shenwl <shenweilun96@gmail.com>
2020-05-08 21:18:05 +0800

    label初始化没有offset，标签重定位之后才有

--- src/main/java/gen/operand/Label.java
+++ src/main/java/gen/operand/Label.java
@@ -3,13 +3,11 @@ package gen.operand;
 public class Label extends Offset {
     String label;
 
+    /**
+     * label初始化没有offset，标签重定位之后才有
+     */
     public Label(String label) {
         super(0);
-        this.label = label;
-    }
-
-    public Label(int offset, String label) {
-        super(offset);
         this.label = label;
     }
 
